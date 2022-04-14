9e9b514519e4ae5c39b950df0077b3121fcfa89b
Starck43 <saloon.as@gmail.com>
2022-04-14 23:23:17 +0300

    Instagram archive book generator. v1.0

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a Instagram parser script by name
+
+import sys, glob
+
+from html_generator import generate_book
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    generate_book('data/')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
